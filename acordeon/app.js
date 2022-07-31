0bc1133cedacc7fca441f4d5dbf0b2d34e088341
Diana Mayorga Portatil <diana101210@gmail.com>
2022-07-31 03:56:51 -0500

    acordeon terminado

--- acordeon/app.js
+++ acordeon/app.js
@@ -1,8 +1,28 @@
 
-const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(function (btn) {
-    btn.addEventListener('click', function(e) {
-        e.currentTarget.parentElement
-    });
-})
+//Seleccionar todo lo que tenga la clase question-item
+const questions = document.querySelectorAll('.question-item');
+
+
+//Hacemos el recorrido de todas las clases encontradas
+questions.forEach(function (question) {
+
+    //Seleccionamos el boton de abrir y cerrar el acordeon
+    const btn = question.querySelector('.question-btn');
+    console.log(btn);
+
+    //Escuchamos el evento clic del botón
+      btn.addEventListener('click', () =>{
+            questions.forEach(function (item) {
+                console.log(question);
+
+                //Si, el elemento elegido es diferente al del question, entonces remueve la clase show-details
+                if(item !== question){
+                    item.classList.remove('show-details');
+                }
+            });
+
+            question.classList.toggle('show-details');
+      });
+    
+});
